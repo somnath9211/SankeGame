06894f66cb553b2555d42f040bd9791690c16f4d
Somnath <somnathmandal6294@gmail.com>
2024-09-02 20:40:59 +0530

    Snake Game

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const gameArena = document.getElementById('game-arena');
+    const arenaSize = 600;
+    const cellSize = 20;
+    let score = 0; // score of the game
+    let gameStarted = false; // game status
+    let food = {x:300, y:200}; // {x:15*20, y:10*20}
+    let snake = [{x:160, y:200}, {x:140, y:200},{x:120, y:200}];// snake array 
+
+    
+
+}) 
